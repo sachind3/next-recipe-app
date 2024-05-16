c52db42d25c1f4896a97edc5ce66ffe38406d5a5
sachind3 <desai.sachin45@gmail.com>
2024-05-16 18:03:23 +0530

    done

--- components/shared/skeletons/ListSkeleton.tsx
+++ components/shared/skeletons/ListSkeleton.tsx
@@ -3,8 +3,8 @@ import CardSkeleton from "./CardSkeleton";
 const ListSkeleton = () => {
   return (
     <div className="grid gap-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 col-span-4">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(() => {
-        return <CardSkeleton />;
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, index: number) => {
+        return <CardSkeleton key={index} />;
       })}
     </div>
   );
